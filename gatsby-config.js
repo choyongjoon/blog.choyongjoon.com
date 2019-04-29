50c579bbfb651403fa1da6321c258feaf641f760
Cho, Yongjoon <w2000wkd@gmail.com>
2019-04-29 22:51:12 +0900

    Update description

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'choyongjoon',
     author: 'Cho, Yongjun',
-    description: 'Personal blog by choyongjoon.',
+    description: '웹 프론트엔드, 음악, 책 등등 by 조용준',
     siteUrl: 'https://blog.choyongjoon.com',
     social: {
       twitter: '@choyongjoon',
@@ -59,77 +59,77 @@ module.exports = {
         trackingId: `UA-139193906-1`,
       },
     },
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
-                const siteUrl = site.siteMetadata.siteUrl
-                const postText = `
-                <div style="margin-top=55px; font-style: italic;">(This is an article posted to my blog at blog.choyongjoon.com. You can read it online by <a href="${siteUrl +
-                  edge.node.fields.slug}">clicking here</a>.)</div>
-              `
+    // {
+    //   resolve: `gatsby-plugin-feed`,
+    //   options: {
+    //     query: `
+    //       {
+    //         site {
+    //           siteMetadata {
+    //             title
+    //             description
+    //             siteUrl
+    //             site_url: siteUrl
+    //           }
+    //         }
+    //       }
+    //     `,
+    //     feeds: [
+    //       {
+    //         serialize: ({ query: { site, allMarkdownRemark } }) => {
+    //           return allMarkdownRemark.edges.map(edge => {
+    //             const siteUrl = site.siteMetadata.siteUrl
+    //             const postText = `
+    //             <div style="margin-top=55px; font-style: italic;">(This is an article posted to my blog at blog.choyongjoon.com. You can read it online by <a href="${siteUrl +
+    //               edge.node.fields.slug}">clicking here</a>.)</div>
+    //           `
 
-                let html = edge.node.html
-                // Hacky workaround for https://github.com/gaearon/overreacted.io/issues/65
-                html = html
-                  .replace(/href="\//g, `href="${siteUrl}/`)
-                  .replace(/src="\//g, `src="${siteUrl}/`)
-                  .replace(/"\/static\//g, `"${siteUrl}/static/`)
-                  .replace(/,\s*\/static\//g, `,${siteUrl}/static/`)
+    //             let html = edge.node.html
+    //             // Hacky workaround for https://github.com/gaearon/overreacted.io/issues/65
+    //             html = html
+    //               .replace(/href="\//g, `href="${siteUrl}/`)
+    //               .replace(/src="\//g, `src="${siteUrl}/`)
+    //               .replace(/"\/static\//g, `"${siteUrl}/static/`)
+    //               .replace(/,\s*\/static\//g, `,${siteUrl}/static/`)
 
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.frontmatter.spoiler,
-                  date: edge.node.fields.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ 'content:encoded': html + postText }],
-                })
-              })
-            },
-            query: `
-              {
-                allMarkdownRemark(
-                  limit: 1000,
-                  sort: { order: DESC, fields: [frontmatter___date] }
-                ) {
-                  edges {
-                    node {
-                      excerpt(pruneLength: 250)
-                      html
-                      fields { 
-                        slug   
-                      }
-                      frontmatter {
-                        title
-                        date
-                        spoiler
-                      }
-                    }
-                  }
-                }
-              }
-            `,
-            output: '/rss.xml',
-            title: "Cho, Yongjun's blog.choyongjoon.com Blog RSS Feed",
-          },
-        ],
-      },
-    },
+    //             return Object.assign({}, edge.node.frontmatter, {
+    //               description: edge.node.frontmatter.spoiler,
+    //               date: edge.node.fields.date,
+    //               url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+    //               guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+    //               custom_elements: [{ 'content:encoded': html + postText }],
+    //             })
+    //           })
+    //         },
+    //         query: `
+    //           {
+    //             allMarkdownRemark(
+    //               limit: 1000,
+    //               sort: { order: DESC, fields: [frontmatter___date] }
+    //             ) {
+    //               edges {
+    //                 node {
+    //                   excerpt(pruneLength: 250)
+    //                   html
+    //                   fields {
+    //                     slug
+    //                   }
+    //                   frontmatter {
+    //                     title
+    //                     date
+    //                     spoiler
+    //                   }
+    //                 }
+    //               }
+    //             }
+    //           }
+    //         `,
+    //         output: '/rss.xml',
+    //         title: "Cho, Yongjun's blog.choyongjoon.com Blog RSS Feed",
+    //       },
+    //     ],
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
